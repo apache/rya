db96c4584815fec6b67f31db2753d12c1bb23f64
pujav65 <pujav65@gmail.com>
2016-01-04 16:31:24 -0500

    RYA-28 adding additional life cycle dependencies for secondary indices

--- dao/accumulo.rya/src/main/java/mvm/rya/accumulo/experimental/AccumuloIndexer.java
+++ dao/accumulo.rya/src/main/java/mvm/rya/accumulo/experimental/AccumuloIndexer.java
@@ -22,12 +22,17 @@ package mvm.rya.accumulo.experimental;
 
 import java.io.IOException;
 
-import mvm.rya.api.persist.index.RyaSecondaryIndexer;
-
+import org.apache.accumulo.core.client.Connector;
 import org.apache.accumulo.core.client.MultiTableBatchWriter;
 
-public interface AccumuloIndexer extends RyaSecondaryIndexer {
-    
-    public void setMultiTableBatchWriter(MultiTableBatchWriter writer) throws IOException;
+import mvm.rya.api.RdfCloudTripleStoreConfiguration;
+import mvm.rya.api.persist.index.RyaSecondaryIndexer;
 
+public interface AccumuloIndexer extends RyaSecondaryIndexer {
+    public void init();    
+    public void setMultiTableBatchWriter(MultiTableBatchWriter writer) throws IOException;
+    public void setConnector(Connector connector);
+    public void destroy();
+    public void purge(RdfCloudTripleStoreConfiguration configuration);
+    public void dropAndDestroy();
 }

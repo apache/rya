1d94f2ef6ab1cbd01358abf9da0e470700f0c46e
Caleb Meier <meiercaleb@gmail.com>
2016-01-07 16:49:16 -0500

    PcjSerializer Stub

---
+++ extras/indexing/src/main/java/mvm/rya/indexing/external/tupleSet/AccumuloPcjSerializer.java
@@ -0,0 +1,17 @@
+package mvm.rya.indexing.external.tupleSet;
+
+import java.util.Set;
+
+import org.openrdf.query.BindingSet;
+
+public class AccumuloPcjSerializer {
+
+	public byte[] serialize(BindingSet bs, Set<String[]> varOrders) {
+		return null;
+	}
+
+	public BindingSet deSerialize(byte[] row) {
+		return null;
+	}
+
+}

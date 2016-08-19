6f0b816c23660259af5b43c2ed243f819102c195
eric.white <eric.white@ads.sparta.com>
2016-08-19 14:40:38 -0400

    Adding licences to files missing it in rya.merger.

--- extras/rya.merger/src/test/java/mvm/rya/accumulo/mr/merge/MergeToolTest.java
+++ extras/rya.merger/src/test/java/mvm/rya/accumulo/mr/merge/MergeToolTest.java
@@ -1,5 +1,25 @@
 package mvm.rya.accumulo.mr.merge;
 
+/*
+ * #%L
+ * mvm.rya.accumulo.mr.merge
+ * %%
+ * Copyright (C) 2014 Rya
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
 import static mvm.rya.accumulo.mr.merge.util.TestUtils.LAST_MONTH;
 import static mvm.rya.accumulo.mr.merge.util.TestUtils.TODAY;
 import static mvm.rya.accumulo.mr.merge.util.TestUtils.YESTERDAY;

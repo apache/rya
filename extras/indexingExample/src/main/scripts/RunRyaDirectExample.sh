9361e0ac521d17b832cac52e4cc21da0431f66f8
eric.white <Eric.White@parsons.com>
2017-09-21 11:11:02 -0400

    RYA-371 Added a .sh script for RyaDirectExample to match the .bat file in the indexingExample distribution.  Added similar scripts to run MongoRyaDirectExample.

---
+++ extras/indexingExample/src/main/scripts/RunRyaDirectExample.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+# 
+#   http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+name="Rya Direct Example"
+echo "Starting $name"
+
+CP=""
+
+# Check to see if javac is on the path
+if which javac >/dev/null; then
+    for f in ./lib/*.jar; do
+        CP+="$f:"
+    done
+
+    echo "Compiling $name"
+    javac -cp "$CP" RyaDirectExample.java
+    echo "Running $name"
+    java -cp "$CP" RyaDirectExample
+
+    echo "Finished $name"
+else
+    echo "ERROR: Could not find javac"
+fi

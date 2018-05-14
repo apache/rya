kchilton2/incubator-rya

RYA-487 Implement Kafka Connect Sink implementations for Accumulo and Mongo DB backed Rya.

extras/kafka.connect/accumulo/src/main/java/org/apache/rya/kafka/connect/accumulo/AccumuloRyaSinkConfig.java
@@ -1,0 +1,98 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.rya.kafka.connect.accumulo;
+
+import static java.util.Objects.requireNonNull;
+
+import java.util.Map;
+
+import org.apache.kafka.common.config.ConfigDef;
+import org.apache.kafka.common.config.ConfigDef.Importance;
+import org.apache.kafka.common.config.ConfigDef.Type;
+import org.apache.kafka.common.config.types.Password;
+import org.apache.rya.kafka.connect.api.sink.RyaSinkConfig;
+
+import edu.umd.cs.findbugs.annotations.DefaultAnnotation;
+import edu.umd.cs.findbugs.annotations.NonNull;
+
+/**
+ * A Kafka Connect configuration that is used to configure {@link AccumuloRyaSinkConnector}s
+ * and {@link AccumuloRyaSinkTask}s.
+ */
+@DefaultAnnotation(NonNull.class)
+public class AccumuloRyaSinkConfig extends RyaSinkConfig {
+
+    public static final String ZOOKEEPERS = "accumulo.zookeepers";
+    private static final String ZOOKEEPERS_DOC = "A comma delimited list of the Zookeeper server hostname/port pairs.";
+
+    public static final String CLUSTER_NAME = "accumulo.cluster.name";
+    private static final String CLUSTER_NAME_DOC = "The name of the Accumulo instance within Zookeeper.";
+
+    public static final String USERNAME = "accumulo.username";
+    private static final String USERNAME_DOC = "The Accumulo username the Sail connections will use.";
+
+    public static final String PASSWORD = "Accumulo.password";
+    private static final String PASSWORD_DOC = "The Accumulo password the Sail connections will use.";
+
+    public static final ConfigDef CONFIG_DEF = new ConfigDef()
+            .define(ZOOKEEPERS, Type.STRING, Importance.HIGH, ZOOKEEPERS_DOC)
+            .define(CLUSTER_NAME, Type.STRING, Importance.HIGH, CLUSTER_NAME_DOC)
+            .define(USERNAME, Type.STRING, Importance.HIGH, USERNAME_DOC)
+            .define(PASSWORD, Type.STRING, Importance.HIGH, PASSWORD_DOC);
+    static {
+        RyaSinkConfig.addCommonDefinitions(CONFIG_DEF);
+    }
+
+    /**
+     * Constructs an instance of {@link AccumuloRyaSinkConfig}.
+     *
+     * @param originals - The key/value pairs that define the configuration. (not null)
+     */
+    public AccumuloRyaSinkConfig(final Map<?, ?> originals) {
+        super(CONFIG_DEF, requireNonNull(originals));
+    }
+
+    /**
+     * @return A comma delimited list of the Zookeeper server hostname/port pairs.
+     */
+    public String getZookeepers() {
+        return super.getString(ZOOKEEPERS);
+    }
+
+    /**
+     * @return The name of the Accumulo instance within Zookeeper.
+     */
+    public String getClusterName() {
+        return super.getString(CLUSTER_NAME);
+    }
+
+    /**
+     * @return The Accumulo username the Sail connections will use.
+     */
+    public String getUsername() {
+        return super.getString(USERNAME);
+    }
+
+    /**
+     * @return The Accumulo password the Sail connections will use.
+     */
+    public Password getPassword() {
+        return super.getPassword(PASSWORD);
+    }
+}

extras/kafka.connect/accumulo/src/main/java/org/apache/rya/kafka/connect/accumulo/AccumuloRyaSinkConnector.java
@@ -1,0 +1,63 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.rya.kafka.connect.accumulo;
+
+import java.util.Map;
+
+import org.apache.kafka.common.config.AbstractConfig;
+import org.apache.kafka.common.config.ConfigDef;
+import org.apache.kafka.connect.connector.Task;
+import org.apache.rya.kafka.connect.api.sink.RyaSinkConnector;
+
+import edu.umd.cs.findbugs.annotations.DefaultAnnotation;
+import edu.umd.cs.findbugs.annotations.NonNull;
+import edu.umd.cs.findbugs.annotations.Nullable;
+
+/**
+ * A {@link RyaSinkConnector} that uses an Accumulo Rya backend when creating tasks.
+ */
+@DefaultAnnotation(NonNull.class)
+public class AccumuloRyaSinkConnector extends RyaSinkConnector {
+
+    @Nullable
+    private AccumuloRyaSinkConfig config = null;
+
+    @Override
+    public void start(final Map<String, String> props) {
+        this.config = new AccumuloRyaSinkConfig( props );
+    }
+
+    @Override
+    protected AbstractConfig getConfig() {
+        if(config == null) {
+            throw new IllegalStateException("The configuration has not been set yet. Invoke start(props) first.");
+        }
+        return config;
+    }
+
+    @Override
+    public Class<? extends Task> taskClass() {
+        return AccumuloRyaSinkTask.class;
+    }
+
+    @Override
+    public ConfigDef config() {
+        return AccumuloRyaSinkConfig.CONFIG_DEF;
+    }
+}

extras/kafka.connect/mongo/src/main/java/org/apache/rya/kafka/connect/mongo/MongoRyaSinkConfig.java
@@ -1,0 +1,95 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.rya.kafka.connect.mongo;
+
+import java.util.Map;
+
+import org.apache.kafka.common.config.ConfigDef;
+import org.apache.kafka.common.config.ConfigDef.Importance;
+import org.apache.kafka.common.config.ConfigDef.Type;
+import org.apache.kafka.common.config.types.Password;
+import org.apache.rya.kafka.connect.api.sink.RyaSinkConfig;
+
+import edu.umd.cs.findbugs.annotations.DefaultAnnotation;
+import edu.umd.cs.findbugs.annotations.NonNull;
+
+/**
+ * A Kafka Connect configuration that is used to configure {@link MongoRyaSinkConnector}s and {@link MongoRyaSinkTask}s.
+ */
+@DefaultAnnotation(NonNull.class)
+public class MongoRyaSinkConfig extends RyaSinkConfig {
+
+    public static final String HOSTNAME = "mongo.hostname";
+    private static final String HOSTNAME_DOC = "The Mongo DB hostname the Sail connections wlll use.";
+
+    public static final String PORT = "mongo.port";
+    private static final String PORT_DOC = "The Mongo DB port the Sail connections will use.";
+
+    public static final String USERNAME = "mongo.username";
+    private static final String USERNAME_DOC = "The Mongo DB username the Sail connections will use.";
+
+    public static final String PASSWORD = "mongo.password";
+    private static final String PASSWORD_DOC = "The Mongo DB password the Sail connections will use.";
+
+    public static final ConfigDef CONFIG_DEF = new ConfigDef()
+            .define(HOSTNAME, Type.STRING, Importance.HIGH, HOSTNAME_DOC)
+            .define(PORT, Type.INT, Importance.HIGH, PORT_DOC)
+            .define(USERNAME, Type.STRING, Importance.HIGH, USERNAME_DOC)
+            .define(PASSWORD, Type.STRING, Importance.HIGH, PASSWORD_DOC);
+    static {
+        RyaSinkConfig.addCommonDefinitions(CONFIG_DEF);
+    }
+
+    /**
+     * Constructs an instance of {@link MongoRyaSinkConfig}.
+     *
+     * @param originals - The key/value pairs that define the configuration. (not null)
+     */
+    public MongoRyaSinkConfig(final Map<?, ?> originals) {
+        super(CONFIG_DEF, originals);
+    }
+
+    /**
+     * @return The Mongo DB hostname the Sail connections wlll use.
+     */
+    public String getHostname() {
+        return super.getString(HOSTNAME);
+    }
+
+    /**
+     * @return The Mongo DB port the Sail connections will use.
+     */
+    public int getPort() {
+        return super.getInt(PORT);
+    }
+
+    /**
+     * @return The Mongo DB username the Sail connections will use.
+     */
+    public String getUsername() {
+        return super.getString(USERNAME);
+    }
+
+    /**
+     * @return The Mongo DB password the Sail connections will use.
+     */
+    public Password getPassword() {
+        return super.getPassword(PASSWORD);
+    }
+}

apache/rya

extras/kafka.connect/accumulo/src/main/java/org/apache/rya/kafka/connect/accumulo/AccumuloRyaSinkConfig.java
@@ -1,0 +1,97 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.rya.kafka.connect.accumulo;
+
+import static java.util.Objects.requireNonNull;
+
+import java.util.Map;
+
+import org.apache.kafka.common.config.ConfigDef;
+import org.apache.kafka.common.config.ConfigDef.Importance;
+import org.apache.kafka.common.config.ConfigDef.Type;
+import org.apache.rya.kafka.connect.api.sink.RyaSinkConfig;
+
+import edu.umd.cs.findbugs.annotations.DefaultAnnotation;
+import edu.umd.cs.findbugs.annotations.NonNull;
+
+/**
+ * A Kafka Connect configuration that is used to configure {@link AccumuloRyaSinkConnector}s
+ * and {@link AccumuloRyaSinkTask}s.
+ */
+@DefaultAnnotation(NonNull.class)
+public class AccumuloRyaSinkConfig extends RyaSinkConfig {
+
+    public static final String ZOOKEEPERS = "accumulo.zookeepers";
+    private static final String ZOOKEEPERS_DOC = "A comma delimited list of the Zookeeper server hostname/port pairs.";
+
+    public static final String CLUSTER_NAME = "accumulo.cluster.name";
+    private static final String CLUSTER_NAME_DOC = "The name of the Accumulo instance within Zookeeper.";
+
+    public static final String USERNAME = "accumulo.username";
+    private static final String USERNAME_DOC = "The Accumulo username the Sail connections will use.";
+
+    public static final String PASSWORD = "accumulo.password";
+    private static final String PASSWORD_DOC = "The Accumulo password the Sail connections will use.";
+
+    public static final ConfigDef CONFIG_DEF = new ConfigDef()
+            .define(ZOOKEEPERS, Type.STRING, Importance.HIGH, ZOOKEEPERS_DOC)
+            .define(CLUSTER_NAME, Type.STRING, Importance.HIGH, CLUSTER_NAME_DOC)
+            .define(USERNAME, Type.STRING, Importance.HIGH, USERNAME_DOC)
+            .define(PASSWORD, Type.PASSWORD, Importance.HIGH, PASSWORD_DOC);
+    static {
+        RyaSinkConfig.addCommonDefinitions(CONFIG_DEF);
+    }
+
+    /**
+     * Constructs an instance of {@link AccumuloRyaSinkConfig}.
+     *
+     * @param originals - The key/value pairs that define the configuration. (not null)
+     */
+    public AccumuloRyaSinkConfig(final Map<?, ?> originals) {
+        super(CONFIG_DEF, requireNonNull(originals));
+    }
+
+    /**
+     * @return A comma delimited list of the Zookeeper server hostname/port pairs.
+     */
+    public String getZookeepers() {
+        return super.getString(ZOOKEEPERS);
+    }
+
+    /**
+     * @return The name of the Accumulo instance within Zookeeper.
+     */
+    public String getClusterName() {
+        return super.getString(CLUSTER_NAME);
+    }
+
+    /**
+     * @return The Accumulo username the Sail connections will use.
+     */
+    public String getUsername() {
+        return super.getString(USERNAME);
+    }
+
+    /**
+     * @return The Accumulo password the Sail connections will use.
+     */
+    public String getPassword() {
+        return super.getPassword(PASSWORD).value();
+    }
+}

extras/kafka.connect/accumulo/src/main/java/org/apache/rya/kafka/connect/accumulo/AccumuloRyaSinkConnector.java
@@ -1,0 +1,66 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.rya.kafka.connect.accumulo;
+
+import static java.util.Objects.requireNonNull;
+
+import java.util.Map;
+
+import org.apache.kafka.common.config.AbstractConfig;
+import org.apache.kafka.common.config.ConfigDef;
+import org.apache.kafka.connect.connector.Task;
+import org.apache.rya.kafka.connect.api.sink.RyaSinkConnector;
+
+import edu.umd.cs.findbugs.annotations.DefaultAnnotation;
+import edu.umd.cs.findbugs.annotations.NonNull;
+import edu.umd.cs.findbugs.annotations.Nullable;
+
+/**
+ * A {@link RyaSinkConnector} that uses an Accumulo Rya backend when creating tasks.
+ */
+@DefaultAnnotation(NonNull.class)
+public class AccumuloRyaSinkConnector extends RyaSinkConnector {
+
+    @Nullable
+    private AccumuloRyaSinkConfig config = null;
+
+    @Override
+    public void start(final Map<String, String> props) {
+        requireNonNull(props);
+        this.config = new AccumuloRyaSinkConfig( props );
+    }
+
+    @Override
+    protected AbstractConfig getConfig() {
+        if(config == null) {
+            throw new IllegalStateException("The configuration has not been set yet. Invoke start(Map) first.");
+        }
+        return config;
+    }
+
+    @Override
+    public Class<? extends Task> taskClass() {
+        return AccumuloRyaSinkTask.class;
+    }
+
+    @Override
+    public ConfigDef config() {
+        return AccumuloRyaSinkConfig.CONFIG_DEF;
+    }
+}

extras/kafka.connect/mongo/src/main/java/org/apache/rya/kafka/connect/mongo/MongoRyaSinkConfig.java
@@ -1,0 +1,94 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.rya.kafka.connect.mongo;
+
+import java.util.Map;
+
+import org.apache.kafka.common.config.ConfigDef;
+import org.apache.kafka.common.config.ConfigDef.Importance;
+import org.apache.kafka.common.config.ConfigDef.Type;
+import org.apache.rya.kafka.connect.api.sink.RyaSinkConfig;
+
+import edu.umd.cs.findbugs.annotations.DefaultAnnotation;
+import edu.umd.cs.findbugs.annotations.NonNull;
+
+/**
+ * A Kafka Connect configuration that is used to configure {@link MongoRyaSinkConnector}s and {@link MongoRyaSinkTask}s.
+ */
+@DefaultAnnotation(NonNull.class)
+public class MongoRyaSinkConfig extends RyaSinkConfig {
+
+    public static final String HOSTNAME = "mongo.hostname";
+    private static final String HOSTNAME_DOC = "The Mongo DB hostname the Sail connections will use.";
+
+    public static final String PORT = "mongo.port";
+    private static final String PORT_DOC = "The Mongo DB port the Sail connections will use.";
+
+    public static final String USERNAME = "mongo.username";
+    private static final String USERNAME_DOC = "The Mongo DB username the Sail connections will use.";
+
+    public static final String PASSWORD = "mongo.password";
+    private static final String PASSWORD_DOC = "The Mongo DB password the Sail connections will use.";
+
+    public static final ConfigDef CONFIG_DEF = new ConfigDef()
+            .define(HOSTNAME, Type.STRING, Importance.HIGH, HOSTNAME_DOC)
+            .define(PORT, Type.INT, Importance.HIGH, PORT_DOC)
+            .define(USERNAME, Type.STRING, "", Importance.HIGH, USERNAME_DOC)
+            .define(PASSWORD, Type.PASSWORD, "", Importance.HIGH, PASSWORD_DOC);
+    static {
+        RyaSinkConfig.addCommonDefinitions(CONFIG_DEF);
+    }
+
+    /**
+     * Constructs an instance of {@link MongoRyaSinkConfig}.
+     *
+     * @param originals - The key/value pairs that define the configuration. (not null)
+     */
+    public MongoRyaSinkConfig(final Map<?, ?> originals) {
+        super(CONFIG_DEF, originals);
+    }
+
+    /**
+     * @return The Mongo DB hostname the Sail connections wlll use.
+     */
+    public String getHostname() {
+        return super.getString(HOSTNAME);
+    }
+
+    /**
+     * @return The Mongo DB port the Sail connections will use.
+     */
+    public int getPort() {
+        return super.getInt(PORT);
+    }
+
+    /**
+     * @return The Mongo DB username the Sail connections will use.
+     */
+    public String getUsername() {
+        return super.getString(USERNAME);
+    }
+
+    /**
+     * @return The Mongo DB password the Sail connections will use.
+     */
+    public String getPassword() {
+        return super.getPassword(PASSWORD).value();
+    }
+}